05d79e00e87e9da6a0e19825453230f9c877778f
Rene Sauerwein <rene@sauerwein.cc>
2020-03-30 10:36:51 +0200

    Implement basic API functionality

--- app/src/main/java/cc/sauerwein/popularmovies_stage1/MainActivity.java
+++ app/src/main/java/cc/sauerwein/popularmovies_stage1/MainActivity.java
@@ -1,8 +1,15 @@
 package cc.sauerwein.popularmovies_stage1;
 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.IOException;
+import java.net.URL;
+
+import cc.sauerwein.popularmovies_stage1.utilities.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadMovieData();
+    }
+
+    private void loadMovieData() {
+        new FetchMovieTask().execute();
+    }
+
+    private class FetchMovieTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            URL movieRequestUrl = NetworkUtils.buildUrl();
+
+            try {
+                String jsonResponse = NetworkUtils.getResponseFromHttpUrl(movieRequestUrl);
+                return jsonResponse;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            TextView tv = findViewById(R.id.tv_test);
+            tv.setText(s);
+        }
     }
 }
