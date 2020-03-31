39170df57e67780fe0d3b00e3038121676315b6f
Rene Sauerwein <rene@sauerwein.cc>
2020-03-31 09:20:37 +0200

    Implement menu in MainActivity

--- app/src/main/java/cc/sauerwein/popularmovies_stage1/MainActivity.java
+++ app/src/main/java/cc/sauerwein/popularmovies_stage1/MainActivity.java
@@ -2,7 +2,11 @@ package cc.sauerwein.popularmovies_stage1;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -72,5 +76,26 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(Movie[] movies) {
             mMovieAdapter.setMovieData(movies);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_top_rated:
+                break;
+            case R.id.action_most_popular:
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
     }
 }
