9187467b91f288b69005968339b54c3e1e19ce95
Rene Sauerwein <rene@sauerwein.cc>
2020-03-31 13:22:36 +0200

    Adjust DetailActivity title

--- app/src/main/java/cc/sauerwein/popularmovies_stage1/DetailActivity.java
+++ app/src/main/java/cc/sauerwein/popularmovies_stage1/DetailActivity.java
@@ -15,6 +15,7 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        setTitle(getString(R.string.movie_detail));
 
         Intent intent = getIntent();
         String json = intent.getStringExtra(Intent.EXTRA_TEXT);
