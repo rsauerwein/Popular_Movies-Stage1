363af531d2480605745e08f0727a4b0be8a033a4
Rene Sauerwein <rene@sauerwein.cc>
2020-04-01 19:39:23 +0200

    Remove unneeded code from NetworkUtils

--- app/src/main/java/cc/sauerwein/popularmovies_stage1/utilities/NetworkUtils.java
+++ app/src/main/java/cc/sauerwein/popularmovies_stage1/utilities/NetworkUtils.java
@@ -2,13 +2,6 @@ package cc.sauerwein.popularmovies_stage1.utilities;
 
 import android.net.Uri;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Scanner;
-
 import cc.sauerwein.popularmovies_stage1.preferences.ApiKey;
 
 public class NetworkUtils {
@@ -24,59 +17,6 @@ public class NetworkUtils {
 
     private static final String DEFAULT_IMAGE_SIZE = "w185";
 
-
-    /**
-     * Builds a API request URL
-     * Format API_BASE_URL + apiPath + API_KEY
-     * Hint: Commonly used apiPaths are stored as public constants within this class
-     *
-     * @param apiPath example: movie/top_rated
-     * @return example: https://api.themoviedb.org/3/movie/top_rated?api_key=xxxx
-     */
-    public static URL buildUrl(String apiPath) {
-        Uri builtUri = Uri.parse(API_BASE_URL).buildUpon()
-                .appendEncodedPath(apiPath)
-                .appendQueryParameter(API_KEY_PARAM, API_KEY)
-                .build();
-
-        URL url = null;
-        try {
-            url = new URL(builtUri.toString());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        return url;
-    }
-
-
-
-    /**
-     * This method returns the entire result from the HTTP response.
-     *
-     * @param url The URL to fetch the HTTP response from.
-     * @return The contents of the HTTP response.
-     * @throws IOException Related to network and stream reading
-     */
-    public static String getResponseFromHttpUrl(URL url) throws IOException {
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-        try {
-            InputStream in = urlConnection.getInputStream();
-
-            Scanner scanner = new Scanner(in);
-            scanner.useDelimiter("\\A");
-
-            boolean hasInput = scanner.hasNext();
-            if (hasInput) {
-                return scanner.next();
-            } else {
-                return null;
-            }
-        } finally {
-            urlConnection.disconnect();
-        }
-    }
 
     /**
      * @param posterPath Stored in the Movie object
